cd0dd59f4ef45d0a57ee45ba8a2762e308187187
Axel CHUINE <axel.chuine.pro@gmail.com>
2023-07-03 17:57:52 +0200

    peuplement de la classe PersonMedicalRecordDto

--- safetynet-alerts/src/main/java/com/safetynetalerts/dto/PersonMedicalRecordDto.java
+++ safetynet-alerts/src/main/java/com/safetynetalerts/dto/PersonMedicalRecordDto.java
@@ -1,4 +1,18 @@
 package com.safetynetalerts.dto;
 
+import java.util.List;
+
 public class PersonMedicalRecordDto {
+
+    private String firstName;
+
+    private String lastName;
+
+    private String phone;
+
+    private Integer age;
+
+    private List<String> medications;
+
+    private List<String> allergies;
 }
