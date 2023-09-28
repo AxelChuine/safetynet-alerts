ed3051b2a7c1dc069d37c03ff773a215b54b9b14
Axel CHUINE <axel.chuine.pro@gmail.com>
2023-09-28 19:05:37 +0200

    bug countAllPersons

--- safetynet-alerts/src/test/java/com/safetynetalerts/services/MedicalRecordServiceTest.java
+++ safetynet-alerts/src/test/java/com/safetynetalerts/services/MedicalRecordServiceTest.java
@@ -1,7 +1,9 @@
 package com.safetynetalerts.services;
 
 import com.safetynetalerts.models.MedicalRecord;
+import com.safetynetalerts.models.Person;
 import com.safetynetalerts.service.IMedicalRecordService;
+import com.safetynetalerts.utils.Data;
 import com.safetynetalerts.utils.Utils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,9 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
@@ -23,6 +27,9 @@ public class MedicalRecordServiceTest {
 
 	@Autowired
 	private IMedicalRecordService service;
+
+	@MockBean
+	private Data data;
 
 
 	@Test
@@ -82,7 +89,7 @@ public class MedicalRecordServiceTest {
 	}
 
 	@Test
-	void getMedicalRecordByUnderageTest() throws IOException {
+	public void getMedicalRecordByUnderageTest() throws IOException {
 		MedicalRecord m = new MedicalRecord();
 		m.setFirstName("Jean");
 		m.setLastName("Dubois");
@@ -93,6 +100,35 @@ public class MedicalRecordServiceTest {
 		when(this.utils.getAllMedicalRecords()).thenReturn(medicalRecords);
 		MedicalRecord mToCompare = this.service.getMedicalRecordByUnderage(m.getFirstName(), m.getLastName());
 		assertEquals(m, mToCompare);
+	}
+
+	@Test
+	public void countAllPersonsTest () throws IOException {
+		Map<String, Integer> mapPersons = new HashMap<>();
+		mapPersons.put("mineurs", 1);
+		mapPersons.put("majeurs", 1);
+		List<MedicalRecord> medicalRecords = new ArrayList<>();
+		MedicalRecord m1 = new MedicalRecord();
+		m1.setBirthDate("09/12/2021");
+		m1.setFirstName("Marc");
+		m1.setLastName("Dubois");
+		MedicalRecord m2 = new MedicalRecord();
+		m2.setBirthDate("09/12/2001");
+		m2.setFirstName("Jean");
+		m2.setLastName("Dubois");
+		medicalRecords.add(m1);
+		medicalRecords.add(m2);
+		Person p1 = new Person.PersonBuilder().firstName("Marc").lastName("Dubois").build();
+		Person p2 = new Person.PersonBuilder().firstName("Jean").lastName("Dubois").build();
+		List<Person> persons = new ArrayList<>();
+		persons.add(p1);
+		persons.add(p2);
+
+
+		when(this.data.getMedicalRecords()).thenReturn(medicalRecords);
+		Map<String, Integer> mapPersonsToCompare = this.service.countAllPersons(persons);
+
+		assertEquals(mapPersons, mapPersonsToCompare);
 	}
 
 
