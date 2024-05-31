053a64dea384cdb67aa625dd55b4460545914daf
Axel CHUINE <axel.chuine.pro@gmail.com>
2024-05-31 10:07:10 +0200

    modification of the method to create a person in the service layer

--- safetynet-alerts/src/main/java/com/safetynetalerts/service/impl/FireStationServiceImpl.java
+++ safetynet-alerts/src/main/java/com/safetynetalerts/service/impl/FireStationServiceImpl.java
@@ -31,6 +31,7 @@ public class FireStationServiceImpl implements IFireStationService {
 
 	private final IFireStationRepository repository;
 
+
 	List<FireStation> fireStations = new ArrayList<>();
 
     public FireStationServiceImpl(IFireStationRepository repository) {
@@ -48,7 +49,7 @@ public class FireStationServiceImpl implements IFireStationService {
 
 	public void createFirestation(FireStationDto pFirestation) {
 		FireStation fireStation = new FireStation(new HashSet<>(pFirestation.getAddresses()), pFirestation.getStationNumber());
-		this.repository.getAllFireStations().add(fireStation);
+		this.repository.createFireStation(fireStation);
 	}
 
 
