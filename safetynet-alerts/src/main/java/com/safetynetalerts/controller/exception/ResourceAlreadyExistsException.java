62051d2c63f9c19c9df57d506b2d86075a21d6ea
Axel CHUINE <axel.chuine.pro@gmail.com>
2024-05-31 10:10:27 +0200

    Creating the resourceAlreadyExistsException

--- safetynet-alerts/src/main/java/com/safetynetalerts/controller/exception/ResourceAlreadyExistsException.java
+++ safetynet-alerts/src/main/java/com/safetynetalerts/controller/exception/ResourceAlreadyExistsException.java
@@ -1,2 +1,17 @@
-package com.safetynetalerts.controller.exception;public class ResourceAlreadyExistsException {
+package com.safetynetalerts.controller.exception;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@NoArgsConstructor
+@Data
+public class ResourceAlreadyExistsException extends Exception {
+    private String message;
+    private HttpStatus status;
+
+    public ResourceAlreadyExistsException(String message) {
+        this.message = message;
+        this.status = HttpStatus.BAD_REQUEST;
+    }
 }
