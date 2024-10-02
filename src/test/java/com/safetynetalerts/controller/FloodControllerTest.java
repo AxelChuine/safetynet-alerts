34a63a6ac217fea8b4f308ab468eb5257a34263c
Axel CHUINE <axel.chuine.pro@gmail.com>
2024-10-02 18:24:57 +0200

    flood controller status ok test est passé

--- src/test/java/com/safetynetalerts/controller/FloodControllerTest.java
+++ src/test/java/com/safetynetalerts/controller/FloodControllerTest.java
@@ -6,6 +6,8 @@ import com.safetynetalerts.dto.MedicalRecordDto;
 import com.safetynetalerts.dto.PersonMedicalRecordDto;
 import com.safetynetalerts.service.MedicalRecordServiceImpl;
 import com.safetynetalerts.service.PersonFirestationServiceImpl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -20,8 +22,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import java.util.ArrayList;
 import java.util.List;
 
-@WebMvcTest(controllers = { FloodController.class })
-@AutoConfigureMockMvc
+@WebMvcTest(controllers = FloodController.class)
 public class FloodControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -32,16 +33,28 @@ public class FloodControllerTest {
     @Mock
     private MedicalRecordServiceImpl medicalRecordService;
 
+    private String badRequest = "No firestation(s) provided";
+    private String station1 = "4";
+    private String station2 = "2";
+    private String firstName = "John";
+    private String lastName = "Doe";
+    private String phone = "04";
+    private Integer age = 5;
+    private List<String> fireStations = List.of(station1, station2);
+    PersonMedicalRecordDto personMedicalRecordDto;
+    private List<PersonMedicalRecordDto> personMedicalRecordDtos = new ArrayList<>();
 
-    String badRequest = "No firestation(s) provided";
+    @BeforeEach
+    public void setUp() throws Exception {
+        personMedicalRecordDto = new PersonMedicalRecordDto(this.firstName, this.lastName, this.phone, this.age, new ArrayList<>(), new ArrayList<>());
+        personMedicalRecordDtos.add(personMedicalRecordDto);
+    }
+
+
 
     @Test
     public void getAllPersonsAndMedicalRecordByFirestationTest () throws Exception {
-        String station1 = "4";
-        String station2 = "2";
-        List<String> fireStations = List.of(station1, station2);
-        List<PersonMedicalRecordDto> personMedicalRecordDtos = new ArrayList<>();
-        Mockito.when(this.service.getPersonsAndMedicalRecordsByFirestation(fireStations)).thenReturn(personMedicalRecordDtos);
+        Mockito.when(this.service.getPersonsAndMedicalRecordsByFirestation(this.fireStations)).thenReturn(personMedicalRecordDtos);
         this.mockMvc.perform(MockMvcRequestBuilders.get("/flood/station")
                         .param("stations", "4,2"))
                 .andExpect(MockMvcResultMatchers.status().isOk());
@@ -51,7 +64,7 @@ public class FloodControllerTest {
     public void getAllPersonsAndMedicalRecordShouldReturnBadRequestIfNoFirestationsAreProvided ()throws Exception {
         Mockito.when(this.service.getPersonsAndMedicalRecordsByFirestation(List.of())).thenThrow(new BadResourceException());
         this.mockMvc.perform(MockMvcRequestBuilders.get("/flood/station")
-                .param("stations", ""))
+                        .param("stations", ""))
                 .andExpect(MockMvcResultMatchers.status().isPreconditionFailed());
     }
 
@@ -59,7 +72,8 @@ public class FloodControllerTest {
     public void getAllPersonsAndMedicalRecordsShouldReturnNotFoundWhenNothingIsFound () throws Exception {
         Mockito.when(this.service.getPersonsAndMedicalRecordsByFirestation(List.of())).thenThrow(new ResourceNotFoundException());
         this.mockMvc.perform(MockMvcRequestBuilders.get("/flood/station")
-                .param("stations", "4,2"))
+                        .param("stations", "4,2"))
                 .andExpect(MockMvcResultMatchers.status().isNotFound());
     }
 }
+
